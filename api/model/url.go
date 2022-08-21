a81bc8b4626d5a179a3ace1e757642a5efd41437
AbdurrahmanA <abdurrahman262@hotmail.com>
2022-08-21 20:38:20 +0300

    feat: added CreatedAt field to URL struct

--- api/model/url.go
+++ api/model/url.go
@@ -1,7 +1,10 @@
 package model
 
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
 type URL struct {
-	OwnerIP     string `bson:"owner_ip"`
-	OriginalURL string `bson:"original_url"`
-	ShortURL    string `bson:"short_url"`
+	OwnerIP     string             `bson:"owner_ip"`
+	OriginalURL string             `bson:"original_url"`
+	ShortURL    string             `bson:"short_url"`
+	CreatedAt   primitive.DateTime `bson:"created_at"`
 }
