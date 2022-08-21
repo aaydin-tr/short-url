bce315b338b99a748fd7cd81698ccd0cc37c93fc
AbdurrahmanA <abdurrahman262@hotmail.com>
2022-08-21 16:53:50 +0300

    feat: added redis helper

---
+++ api/pkg/redis/redis.go
@@ -0,0 +1,46 @@
+package redis
+
+import (
+	"context"
+	"sync"
+	"time"
+
+	"github.com/go-redis/redis"
+)
+
+type Redis struct {
+	RDB *redis.Client
+	CTX context.Context
+}
+
+var redisClient *redis.Client
+var doOnce sync.Once
+
+func NewRedisClient(URL string, Password string) *Redis {
+	context := context.Background()
+
+	doOnce.Do(func() {
+		redisClient = redis.NewClient(&redis.Options{
+			Addr:     URL,
+			Password: Password,
+		})
+
+	})
+
+	return &Redis{
+		RDB: redisClient,
+		CTX: context,
+	}
+}
+
+func (r *Redis) Ping() error {
+	return r.RDB.Ping().Err()
+}
+
+func (r *Redis) Set(key string, value string, ttl time.Duration) error {
+	return r.RDB.Set(key, value, ttl).Err()
+}
+
+func (r *Redis) Get(key string) (string, error) {
+	return r.RDB.Get(key).Result()
+}
