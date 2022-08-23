ae2b9a951cab4376f65f07868d35197d5c2a8951
AbdurrahmanA <abdurrahman262@hotmail.com>
2022-08-23 23:15:26 +0300

    refactor: added msgForTag, RedirectShortURLValidation and some refactoring

--- server/api/request/request.go
+++ server/api/request/request.go
@@ -1,27 +1,9 @@
 package request
 
-import (
-	"github.com/AbdurrahmanA/short-url/api/response"
-	"github.com/go-playground/validator/v10"
-)
-
 type NewURL struct {
 	URL string `json:"url" validate:"required,url"`
 }
 
-var validate = validator.New()
-
-func ValidateNewURLRequest(url NewURL) []*response.ValidationMessage {
-	var errors []*response.ValidationMessage
-	err := validate.Struct(url)
-	if err != nil {
-		for _, err := range err.(validator.ValidationErrors) {
-			var element response.ValidationMessage
-			element.FailedField = err.Field()
-			element.Tag = err.Tag()
-			element.Message = err.Error()
-			errors = append(errors, &element)
-		}
-	}
-	return errors
+type GetURL struct {
+	URL string `validate:"required,len=8,alphanum"`
 }
