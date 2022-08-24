16e85618315c53c34b8743852ce92d0a3d245a98
AbdurrahmanA <abdurrahman262@hotmail.com>
2022-08-24 21:22:34 +0300

    feat: Added _id field

--- server/model/url.go
+++ server/model/url.go
@@ -3,6 +3,7 @@ package model
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type URL struct {
+	ID          primitive.ObjectID `bson:"_id"`
 	OwnerIP     string             `bson:"owner_ip"`
 	OriginalURL string             `bson:"original_url"`
 	ShortURL    string             `bson:"short_url"`
