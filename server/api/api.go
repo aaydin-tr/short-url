a956a408510432df7a376c50606e3949833f1f3e
AbdurrahmanA <abdurrahman262@hotmail.com>
2022-08-24 20:14:43 +0300

    feat: Added limiterHandler func and using it for limit ip based authentication

--- server/api/api.go
+++ server/api/api.go
@@ -1,10 +1,13 @@
 package api
 
 import (
+	"time"
+
 	"github.com/AbdurrahmanA/short-url/api/middleware"
 	"github.com/AbdurrahmanA/short-url/api/response"
 	"github.com/AbdurrahmanA/short-url/api/routes"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/limiter"
 )
 
 func errorHandler(c *fiber.Ctx, err error) error {
@@ -20,7 +23,14 @@ func errorHandler(c *fiber.Ctx, err error) error {
 	})
 }
 
-func InitAPI(port string, routes *routes.Routes) {
+func limiterHandler(c *fiber.Ctx) error {
+	return c.Status(fiber.StatusTooManyRequests).JSON(response.ErrorResponse{
+		Message: "Too many attempts please try again later",
+		Status:  fiber.StatusTooManyRequests,
+	})
+}
+
+func InitAPI(port string, userHourlyLimit int, routes *routes.Routes) {
 	app := fiber.New(
 		fiber.Config{
 			ErrorHandler: errorHandler,
@@ -28,7 +38,7 @@ func InitAPI(port string, routes *routes.Routes) {
 	)
 
 	app.Get("/+", middleware.RedirectShortURLValidation, routes.RedirectShortURL)
-	app.Put("/", middleware.CreateNewShortURLValidation, routes.CreateNewShortURL)
+	app.Put("/", limiter.New(limiter.Config{Max: userHourlyLimit, LimitReached: limiterHandler, Duration: time.Hour}), middleware.CreateNewShortURLValidation, routes.CreateNewShortURL)
 
 	app.Listen(":" + port)
 }
