4fae0d8cd162ec22f928d35e648e3796058c3846
AbdurrahmanA <abdurrahman262@hotmail.com>
2022-08-27 21:06:57 +0300

    feat: Added Link component

---
+++ web/src/lib/components/Link.svelte
@@ -0,0 +1,10 @@
+<script>
+  import { CardText } from "svelte-materialify";
+  export let message, short_url;
+</script>
+
+<CardText>
+  {message}
+  <br />
+  <a href={short_url} target="_blank">{short_url}</a>
+</CardText>
