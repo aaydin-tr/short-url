43a69238d0bf14149cad628a8854f2d2e6aa1800
AbdurrahmanA <abdurrahman262@hotmail.com>
2022-08-27 21:06:48 +0300

    feat: Added Input component

---
+++ web/src/lib/components/Input.svelte
@@ -0,0 +1,13 @@
+<script>
+  import { TextField } from "svelte-materialify";
+  export let error, messages, url, onKeyPress;
+</script>
+
+<TextField
+  {...$$restProps}
+  messages={(error && messages) || []}
+  success={!error}
+  {error}
+  bind:value={url}
+  on:keypress={onKeyPress}
+/>
